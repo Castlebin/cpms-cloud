6531c3a0808329d68faf916e6a486b7338a6d8c2
gulang12 <15889745718@163.com>
2021-06-20 19:25:02 +0800

    熔断超时配置

--- cpms-service/cpms-system/src/main/java/com/cpms/system/common/enums/SystemResponseResultEnum.java
+++ cpms-service/cpms-system/src/main/java/com/cpms/system/common/enums/SystemResponseResultEnum.java
@@ -4,19 +4,22 @@ import com.cpms.common.core.api.IResultCode;
 
 /**
  * @description:
- * @author: 01396003
+ * @author: gulang
  * @time: 2021/5/25 15:49
  */
 public enum SystemResponseResultEnum implements IResultCode {
     ACCOUNT_NOT_EXIST_ERROR(50001, "账号不存在"),
-    PASSWORD_VERIFICATION_ERROR(50002, "密码校验错误");
+    PASSWORD_VERIFICATION_ERROR(50002, "密码校验错误"),
+    ;
 
     final Integer code;
     final String message;
+
     @Override
     public Integer getCode() {
         return code;
     }
+
     @Override
     public String getMessage() {
         return message;
