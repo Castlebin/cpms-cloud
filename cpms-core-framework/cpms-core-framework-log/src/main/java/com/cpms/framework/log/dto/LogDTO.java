d39d6b185e8b0d964de26f701b86a0f7b46a9d00
gulang12 <15889745718@163.com>
2021-09-05 16:45:04 +0800

    代码优化

---
+++ cpms-core-framework/cpms-core-framework-log/src/main/java/com/cpms/framework/log/dto/LogDTO.java
@@ -0,0 +1,29 @@
+package com.cpms.framework.log.dto;
+
+import lombok.Data;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+/**
+ * @description: 操作日志实体类
+ * @author: gulang
+ * @time: 2021/8/18 17:14
+ */
+@Data
+@ToString
+public class LogDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String title;
+    private Long   tenantId;
+    private String serviceName;
+    private String handleIp;
+    private String reqUrl;
+    private String reqMethod;
+    private String reqParams;
+    private String createBy;
+    private Long   exeTime;
+    private String resultMsg;
+    private String className;
+    private String methodName;
+}
---
+++ cpms-core-framework/cpms-core-framework-log/src/main/java/com/cpms/framework/log/handler/ResponseIntercept.java
@@ -0,0 +1,49 @@
+package com.cpms.framework.log.handler;
+
+import com.alibaba.fastjson.JSON;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author gulang
+ * @Description:
+ * @time: 2021/8/22 14:51
+ */
+@ControllerAdvice
+public class ResponseIntercept implements ResponseBodyAdvice<Object> {
+    /**
+     * 请求头设置返回json
+     */
+    public static final String RESPONSE_JSON = "_response_json";
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    /**
+     * 此方法是拦截返回值,并且操作返回值的,这是一个全局过滤
+     * @param body
+     * @param returnType
+     * @param selectedContentType
+     * @param selectedConverterType
+     * @param request
+     * @param response
+     * @return
+     */
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        ServletServerHttpRequest req=(ServletServerHttpRequest)request;
+        HttpServletRequest servletRequest = req.getServletRequest();
+        servletRequest.setAttribute(RESPONSE_JSON, JSON.toJSONString(body));
+        return body;
+    }
+}
