e85c6173142f04414a91db0ae2e473d0463aa118
01396003 <liuzaichun@sfmail.sf-express.com>
2021-12-20 17:45:38 +0800

    <优化:> 1.整合异常捕获

--- cpms-service/cpms-log/src/main/java/com/cpms/log/exception/LogExceptionHandler.java
+++ cpms-service/cpms-log/src/main/java/com/cpms/log/exception/LogExceptionHandler.java
@@ -21,22 +21,4 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 @RestControllerAdvice
 public class LogExceptionHandler extends GlobalExceptionHandler {
 
-    /**
-     * 捕获资源不存在异常NoHandlerFoundException [gateway 网关层不能引入该异常类，不支持servlet，否则报错]
-     */
-    @ExceptionHandler(NoHandlerFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Result<Void> noHandlerFoundException(NoHandlerFoundException e) {
-        log.error("捕获NoHandlerFoundException异常信息:",e);
-        return ResultUtil.error(GlobalResponseResultEnum.NO_HANDLER_FOUND_ERROR.getCode(),e.getMessage());
-    }
-
-    /**
-     * 请求方式异常
-     */
-    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
-    public Result<Object> httpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
-        return ResultUtil.error(GlobalResponseResultEnum.METHOD_NOT_SUPPORTED_ERROR.getCode(),e.getMessage(),null,null);
-    }
 }
