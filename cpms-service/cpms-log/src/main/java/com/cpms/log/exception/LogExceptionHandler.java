179509fdf8036fa23612cd9ef12a68be2ca6d217
01396003 <liuzaichun@sfmail.sf-express.com>
2021-12-21 15:32:40 +0800

    <优化:> 1.全局异常捕获代码整合

--- cpms-service/cpms-log/src/main/java/com/cpms/log/exception/LogExceptionHandler.java
+++ cpms-service/cpms-log/src/main/java/com/cpms/log/exception/LogExceptionHandler.java
@@ -1,16 +1,8 @@
 package com.cpms.log.exception;
 
-import com.cpms.framework.common.core.api.Result;
-import com.cpms.framework.common.core.api.ResultUtil;
-import com.cpms.framework.common.enums.GlobalResponseResultEnum;
 import com.cpms.framework.common.exception.GlobalExceptionHandler;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.NoHandlerFoundException;
 
 /**
  * @description: 日志服务异常捕获
